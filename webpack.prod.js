74dfb4347d0c54f801235a7fe77f5e37fa994457
Michael Mirisciotta <151809512+Michaelmira@users.noreply.github.com>
2025-05-10 01:20:56 +0000

    debug dist

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,12 @@
-// webpack.prod.js
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const Dotenv = require('dotenv-webpack');
+
 module.exports = merge(common, {
     mode: 'production',
+    output: {
+        publicPath: '/' // Explicitly set public path
+    },
     plugins: [
         new Dotenv({
             safe: true,
